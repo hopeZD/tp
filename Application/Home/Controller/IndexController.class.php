d78f5e48c1df9b55d6f6d0406ba69bda0e07cb50
hope <zhengzuodi@gmail.com>
2016-08-08 00:51:34 +0800

    add userself

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -34,12 +34,47 @@ class IndexController extends Controller {
    		$this->logger("发送消息: \n".$text);
    		$wechat->replyText($text);
 
+   	} else if (strstr($data['Content'], 'myself')) {
+   		$this->users($wechat, $data);
    	}
 
    } 
 
+   private function users($wechat, $data) {
+
+   		$openid = $data['FromUserName'];
+   		$appID = "wx8b1b083b085166b5";
+   		$appSecret = "359161300729b69553b91aa2a0dcd5c6";
+   		$token = session('token');
+
+   		if($token) {
+
+   			$wechatAuth = new WechatAuth($appID, $appSecret, $token);
+
+   		} else {
+
+   			$wechatAuth = new WechatAuth($appID, $appSecret);
+
+	   		$accsseToken = $wechat->getAccessToken();
+	   		$token = $accsseToken['access_token'];
+	   		session('token', $token);
+
+   		}
+
+   		$user = $wechatAuth->userInfo($openid);
+   		$text = "你的openid是：".$user['openid']."\n你的昵称是：".$user['nickname']."\n 你的姓别是：".$user['sex']."\n你所在城市是：".$user['city']."\n 你所在国家是： ".$user['country']."\n你所在的省份是：".$user['provice'];
+
+
+   		$this->logger("发送用户的信息");
+   		$wechat->replyText($text);
+
+   		
+
+   }
+
    //日志
    private function logger($content) {
+   	
    	$logSize = 100000;
    	$log = "log.txt";
 
